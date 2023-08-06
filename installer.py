3fdaa5fb39508516d258563cb284ba1e6257c59f
CurrentlyIsGlitching <126282768+CurrentlyIsGlitching@users.noreply.github.com>
2023-08-06 17:23:38 +0500

    Update installer.py

--- installer.py
+++ installer.py
@@ -6,7 +6,7 @@ try:
     start = start.lower()
     if start == "y":
         print("uninstalling...")
-        os.system("rm -r /.notes/ && rm /bin/GlitchNotes")
+        os.system("rm -r /.glitchnotes/ && rm /bin/GlitchNotes")
     else:
         pass
 except FileNotFoundError:
@@ -14,25 +14,6 @@ except FileNotFoundError:
     start = start.lower()
     if start == "y":
         print("starting installation...")
-        os.system("mkdir /.glitchnotes/ && mkdir /.glitchnotes/notes && touch /.glitchnotesnotes/installer && echo \"python /.glitchnotes/main.py\" > /bin/glitchnotes && chmod +x /bin/notes && git clone https://github.com/CurrentlyIsGlitching/Glitchnotes/ && cp Glitchnotes/main.py /.glitchnotes/ && rm -r Glitchnotes &&echo \"installation succesful! if you want to launch GlitchNotes just type glitchnotes, and if you want to remove this app open this installer again.\"")
-    else:
-[admin@localhost заметки]$ cat installer.py
-import oscalhost заметки]$ cat installer.py
-try:
-    file = open("/.glitchnotes/installer", "r")
-    file.close()
-    start = input("are you sure you want to uninstall GlitchNotes? (your notes will be deleted!) [y/n]")
-    start = start.lower()
-    if start == "y":
-        print("uninstalling...")
-        os.system("rm -r /.notes/ && rm /bin/GlitchNotes")
-    else:
-        pass
-except FileNotFoundError:
-    start = input("are you sure you want to install GlitchNotes? (for installer to work you need these packages: git) (only works on linux) [y/n] ")
-    start = start.lower()
-    if start == "y":
-        print("starting installation...")
-        os.system("mkdir /.glitchnotes/ && mkdir /.glitchnotes/notes && touch /.glitchnotesnotes/installer && echo \"python /.glitchnotes/main.py\" > /bin/glitchnotes && chmod +x /bin/notes && git clone https://github.com/CurrentlyIsGlitching/Glitchnotes/ && cp Glitchnotes/main.py /.glitchnotes/ && rm -r Glitchnotes &&echo \"installation succesful! if you want to launch GlitchNotes just type glitchnotes, and if you want to remove this app open this installer again.\"")
+        os.system("mkdir /.glitchnotes/ && mkdir /.glitchnotes/notes && touch /.glitchnotesnotes/installer && echo \"python /.glitchnotes/main.py\" > /bin/glitchnotes && chmod +x /bin/notes && git clone https://github.com/CurrentlyIsGlitching/Glitchnotes/ && cp Glitchnotes/main.py /.glitchnotes/ && rm -r Glitchnotes && echo \"installation succesful! if you want to launch GlitchNotes just type glitchnotes, and if you want to remove this app open this installer again.\"")
     else:
         print("cancelling...")
